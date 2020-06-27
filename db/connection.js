1b2a0a21fb94999dd6c5ba803c653eb97335a738
ashaw916 <a.s.young530@gmail.com>
2020-06-27 14:12:39 -0700

    fixed typo

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "notes_db",
-});
+// JAWSDB_URL
+let connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "notes_db",
+      });
+}
+
 
 // create connection to mysql
 connection.connect();
